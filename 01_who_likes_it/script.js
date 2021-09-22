c02a93b3aae74b579d009f04d02c7520c0b59733
mconour <michaelconour@gmail.com>
2021-09-22 11:43:46 -0700

    complete likes function

--- 01_who_likes_it/script.js
+++ 01_who_likes_it/script.js
@@ -1,4 +1,4 @@
-function likes(names) {
+/* const likes = (names) => {
     return {
         0: `no one likes this`,
         1: `${names[0]} likes this`,
@@ -6,10 +6,20 @@ function likes(names) {
         3: `${names[0]}, ${names[1]} and ${names[2]} like this`,
         4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
     } [Math.min(4, names.length)]
+} */
+
+const likes = (name) => {
+    return {
+        0: `no one likes this`,
+        1: `${name[0]} likes this`,
+        2: `${name[0]} and ${name[1]} like this`,
+        3: `${name[0]}, ${name[1]} and ${name[2]} like this`,
+        4: `${name[0]}, ${name[1]} and ${name.length - 2} others like this`
+    } [Math.min(4, name.length)]
 }
 
-console.log([]); // -->  "no one likes this"
-console.log(["Peter"]); // -->  "Peter likes this"
-console.log(["Jacob", "Alex"]); // -->  "Jacob and Alex like this"
-console.log(["Max", "John", "Mark"]); // -->  "Max, John and Mark like this"
-console.log(["Alex", "Jacob", "Mark", "Max"]); // -->  "Alex, Jacob and 2 others like this"
+console.log(likes([])); // -->  "no one likes this"
+console.log(likes(["Peter"])); // -->  "Peter likes this"
+console.log(likes(["Jacob", "Alex"])); // -->  "Jacob and Alex like this"
+console.log(likes(["Max", "John", "Mark"])); // -->  "Max, John and Mark like this"
+console.log(likes(["Alex", "Jacob", "Mark", "Max"])); // -->  "Alex, Jacob and 2 others like this"
